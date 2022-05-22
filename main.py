7163a2c277a2769fd8e4b0726783fb601a03e66d
Burtek <73553059+Venomiks@users.noreply.github.com>
2022-05-22 23:58:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ class Cards:
     play = [1, 2, 3, 4, 5, 6, 7, 8, 9, ace_random, king_queen_jack]
 
     cards_in_hand = [random.choice(play), random.choice(play)]
+    #  opponents cards etc
+    opponent = [random.choice(play), random.choice(play)]
+
 cards = Cards()
 
-class Opponent(Cards):
+print(f'oponents cards: {cards.opponent[0]} {cards.opponent[1]}')
 
-    cards_in_hand_2 = [random.choice(cards.play), random.choice(cards.play)]
-    print(f'oponents cards: {cards_in_hand_2[0]} {cards_in_hand_2[1]}')
+sum_of_cards_2 = sum(cards.opponent)
 
-    sum_of_cards_2 = sum(cards_in_hand_2)
-
-opponent = Opponent()
 
 
 deck = []
+op_deck = []
 
 #iterating through cards and modifying deck
 for i in cards.cards_in_hand:
@@ -35,30 +35,32 @@ for i in cards.cards_in_hand:
         deck.append(random.choice(cards.tens))
     else:
         deck.append(i)
-hand = f'your cards: {deck[:]}'
-
+print(f'your cards: {deck[:]}')
 
 # summing the cards value
-sum_of_cards = sum(cards.cards_in_hand)
-print(hand)
+sum_of_cards = sum(deck[0:])
+print(f'sum of your cards: {sum_of_cards}')
 
 def cards_in_play():
     # checking the sum of cards in hand, if it reaches over 21 then you lose
-    if opponent.sum_of_cards_2 < sum_of_cards:
+    if sum_of_cards_2 < sum_of_cards:
+        print("you win")
+    elif sum_of_cards == 21:
         print("you win")
     elif sum_of_cards > 21:
-        print("you win")
-    else:
         print("you lose")
-    print(f'sum of your cards is: {sum_of_cards}')
+    # print(f'sum of your cards is: {sum_of_cards}')
 
 
 # cheking for player to pick more cards
 choice = input("Wanna pick more cards? yes/no")
+# for j in choice:
 if choice == "yes":
-    deck.append(random.choice(cards.tens))
+    deck.append(random.choice(cards.play))
+    print(f'your cards: {deck[0:]}')
+    print(f'sum of your cards: {sum(deck)}')
 else:
     pass
-print(hand)
+# print(deck)
 
 cards_in_play()
