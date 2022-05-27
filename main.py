5b39c09378eff095df8ffe3373dba0eadb7e40d3
Burtek <73553059+Venomiks@users.noreply.github.com>
2022-05-27 21:37:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ class Cards:
 
     king_queen_jack = 10
 
-    tens = ["king", "queen", "jack"]
-
     play = [1, 2, 3, 4, 5, 6, 7, 8, 9, ace_random, king_queen_jack]
 
     cards_in_hand = [random.choice(play), random.choice(play)]
@@ -20,8 +18,6 @@ cards = Cards()
 
 print(f'oponents cards: {cards.opponent[0]} {cards.opponent[1]}')
 
-sum_of_cards_2 = sum(cards.opponent)
-
 
 
 deck = []
@@ -30,37 +26,45 @@ op_deck = []
 #iterating through cards and modifying deck
 for i in cards.cards_in_hand:
     if i == 11 or i == 1:
-        deck.append('ace')
+        deck.append(random.choice(cards.ace))
     elif i == 10:
-        deck.append(random.choice(cards.tens))
+        deck.append(cards.king_queen_jack)
     else:
         deck.append(i)
-print(f'your cards: {deck[:]}')
+
+#iterating through cards and modifying deck
+for j in cards.opponent:
+    if j == 11 or j == 1:
+        op_deck.append(random.choice(cards.ace))
+    elif j == 10:
+        op_deck.append(cards.king_queen_jack)
+    else:
+        op_deck.append(j)
+
 
 # summing the cards value
-sum_of_cards = sum(deck[:])
-print(f'sum of your cards: {sum_of_cards}')
+print(f'your cards: {deck}')
+print(f'sum of your cards: {sum(deck[:])}')
 
-def cards_in_play():
-    # checking the sum of cards in hand, if it reaches over 21 then you lose
-    if sum_of_cards_2 < sum_of_cards:
-        print("you win")
-    elif sum_of_cards == 21:
-        print("you win")
-    elif sum_of_cards > 21:
-        print("you lose")
-    # print(f'sum of your cards is: {sum_of_cards}')
-
+choice = input("Wanna pick more cards? yes/no")
 
 # cheking for player to pick more cards
-choice = input("Wanna pick more cards? yes/no")
-# for j in choice:
 if choice == "yes":
     deck.append(random.choice(cards.play))
     print(f'your cards: {deck[:]}')
-    print(f'sum of your cards: {sum(deck)}')
-else:
-    pass
-# print(deck)
+    print(f'your opponents cards: {op_deck[:]}')
+    print(f'sum of your cards: {sum(deck[:])}')
+
+def cards_in_play():
+    # checking the sum of cards in hand, if it reaches over 21 then you lose
+    if sum(op_deck[:]) < sum(deck[:]):
+        print("you win")
+    elif sum(deck[:]) == 21:
+        print("you win")
+    elif sum(deck[:]) != 21 and sum(op_deck[:]) > sum(deck[:]):
+        print("you lose")
+    # elif sum(op_deck[:]) > sum(deck[:]):
+    #     print("you lose")
+
 
 cards_in_play()
