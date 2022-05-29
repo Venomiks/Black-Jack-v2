34a59b9c9269e545dfb7d6d9b871df5b0acb29ad
Burtek <73553059+Venomiks@users.noreply.github.com>
2022-05-29 23:28:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -16,56 +16,64 @@ class Cards:
 
 cards = Cards()
 
-print(f'oponents cards: {cards.opponent[0]} {cards.opponent[1]}')
+
+while True:
+
+    deck = []
+    op_deck = []
 
 
 
-deck = []
-op_deck = []
+    #iterating through cards and modifying deck
+    for i in cards.cards_in_hand:
+        if i == 11 or i == 1:
+            deck.append(random.choice(cards.ace))
+        elif i == 10:
+            deck.append(cards.king_queen_jack)
+        else:
+            deck.append(i)
 
-#iterating through cards and modifying deck
-for i in cards.cards_in_hand:
-    if i == 11 or i == 1:
-        deck.append(random.choice(cards.ace))
-    elif i == 10:
-        deck.append(cards.king_queen_jack)
+
+    #iterating through cards and modifying deck
+    for j in cards.opponent:
+        if j == 11 or j == 1:
+            op_deck.append(random.choice(cards.ace))
+        elif j == 10:
+            op_deck.append(cards.king_queen_jack)
+        else:
+            op_deck.append(j)
+
+    # summing the cards value
+    print(f'oponents cards: {op_deck[:]}')
+    print(f'your cards: {deck}')
+
+    choice = input("Wanna pick more cards? yes/no")
+
+    # cheking for player to pick more cards
+    if choice == "yes":
+        deck.append(random.choice(cards.play))
+        op_deck.append(random.choice(cards.play))
+        print(f'your cards: {deck[:]}')
+        print(f'your opponents cards: {op_deck[:]}')
     else:
-        deck.append(i)
+        pass
 
-#iterating through cards and modifying deck
-for j in cards.opponent:
-    if j == 11 or j == 1:
-        op_deck.append(random.choice(cards.ace))
-    elif j == 10:
-        op_deck.append(cards.king_queen_jack)
-    else:
-        op_deck.append(j)
-
-
-# summing the cards value
-print(f'your cards: {deck}')
-print(f'sum of your cards: {sum(deck[:])}')
-
-choice = input("Wanna pick more cards? yes/no")
-
-# cheking for player to pick more cards
-if choice == "yes":
-    deck.append(random.choice(cards.play))
-    print(f'your cards: {deck[:]}')
-    print(f'your opponents cards: {op_deck[:]}')
-    print(f'sum of your cards: {sum(deck[:])}')
-
-# Win/Lose "screen"
-def cards_in_play():
-    # checking the sum of cards in hand, if it reaches over 21 then you lose
-    if sum(op_deck[:]) < sum(deck[:]):
-        print("you win")
-    elif sum(deck[:]) == 21:
-        print("you win")
-    elif sum(deck[:]) != 21 and sum(op_deck[:]) > sum(deck[:]):
-        print("you lose")
-    # elif sum(op_deck[:]) > sum(deck[:]):
-    #     print("you lose")
+    def cards_in_play():
+        # checking the sum of cards in hand, if it reaches over 21 then you lose
+        if sum(op_deck[:]) < sum(deck[:]) and sum(deck[:]) < 21 or sum(deck[:]) == 21:
+            print("you win")
+        elif sum(deck[:]) > 21:
+            print("you lose")
+        else:
+            print("you lose")
 
 
-cards_in_play()
+    cards_in_play()
+    answer = input("wanna play again? yes/no: ")
+
+    if answer == 'no':
+        break
+
+
+
+
